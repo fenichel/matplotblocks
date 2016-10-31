c096b453faab898fd951d64d6bef96b7eec7f590
fenichel <fenichr@gmail.com>
2016-10-30 23:15:47 -0700

    More blocks, more impasses.

--- matplotlib_blocks_1.js
+++ matplotlib_blocks_1.js
@@ -1,9 +1,11 @@
 Blockly.Blocks['create_graph'] = {
   init: function() {
-    this.appendValueInput("NAME")
-        .setCheck("String")
-        .appendField("Create graph with filename");
-    this.setNextStatement(true, "configure_plot");
+    this.appendDummyInput()
+        .appendField("Create graph with ")
+        .appendField(new Blockly.FieldDropdown([["1 y scale", "1"], ["2 y scales", "2"]]), "NUM_Y_AXES");
+    this.appendStatementInput("NAME")
+        .setCheck(null);
+    this.setNextStatement(true, null);
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
@@ -58,6 +60,7 @@ Blockly.Blocks['add_data_two'] = {
         .setCheck("configure_dataseries");
     this.setPreviousStatement(true, "configure_plot");
     this.setNextStatement(true, "configure_plot");
+    this.setColour(260);
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
@@ -112,6 +115,7 @@ Blockly.Blocks['dataseries_set_color'] = {
     this.setPreviousStatement(true, "configure_dataseries");
     this.setNextStatement(true, "configure_dataseries");
     this.setTooltip('');
+    this.setColour(120);
     this.setHelpUrl('http://www.example.com/');
   }
 };
@@ -124,6 +128,7 @@ Blockly.Blocks['dataseries_set_linewidth'] = {
     this.setPreviousStatement(true, "configure_dataseries");
     this.setNextStatement(true, "configure_dataseries");
     this.setTooltip('');
+    this.setColour(120);
     this.setHelpUrl('http://www.example.com/');
   }
 };
@@ -132,11 +137,12 @@ Blockly.Blocks['dataseries_set_linestyle'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("set line style")
-        .appendField(new Blockly.FieldDropdown([["solid", "-"], ["dashed", "--"], ["dashdot", "-."], ["dotted", ":"], ["don't draw", "None"]]), "STYLE");
+        .appendField(new Blockly.FieldDropdown([["solid", "-"], ["dashed", "--"], ["dashdot", "-."], ["dotted", ":"], ["only points", "None"]]), "STYLE");
     this.setPreviousStatement(true, "configure_dataseries");
     this.setNextStatement(true, "configure_dataseries");
     this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+    this.setColour(120);
+    this.setHelpUrl('http://matplotlib.org/1.5.3/api/lines_api.html#matplotlib.lines.Line2D.set_linestyle');
   }
 };
 
@@ -144,9 +150,100 @@ Blockly.Blocks['dataseries_set_marker'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("set marker style")
-        .appendField(new Blockly.FieldDropdown([["point", "."], ["square", "s"], ["circle", "o"], ["x", "x"], ["plus", "+"], ["triangle down", "v"], ["no marker", "None"], ["octagon", "8"], ["pentagon", "p"], ["star", "*"], ["pixel", ","], ["hexagon1", "h"], ["diamond", "D"], ["thin diamond", "d"], ["vline", "|"], ["hline", "_"], ["tick left", "TICKLEFT"], ["tick right", "TICKRIGHT"], ["tick up", "TICKUP"], ["tick down", "TICKDOWN"], ["caret left", "CARETLEFT"], ["caret right", "CARETRIGHT"], ["caret up", "CARETUP"], ["caret down", "CARETDOWN"], ["triangle up", "^"], ["triangle left", "<"], ["triangle right", ">"], ["tri down", "1"], ["tri up", "2"], ["tri left", "3"], ["tri right", "4"]]), "MARKER");
+        .appendField(new Blockly.FieldDropdown([["point", "."], ["square", "s"], ["circle", "o"], ["x", "x"], ["plus", "+"], ["triangle down", "v"], ["no marker", "None"], ["octagon", "8"], ["pentagon", "p"], ["star", "*"], ["pixel", ","], ["hexagon1", "h"], ["diamond", "D"], ["thin diamond", "d"], ["vline", "|"], ["hline", "_"], ["tick left", "TICKLEFT"], ["tick right", "TICKRIGHT"], ["tick up", "TICKUP"], ["tick down", "TICKDOWN"], ["caret left", "CARETLEFT"], ["caret right", "CARETRIGHT"], ["caret up", "CARETUP"], ["caret down", "CARETDOWN"], ["triangle up", "^"], ["triangle left", "<"], ["triangle right", ">"], ["tri down", "1"], ["tri up", "2"], ["tri left", "3"], ["tri right", "4"]]), "MARKER")
+        .appendField("size")
+        .appendField(new Blockly.FieldNumber(1), "MARKER_SIZE");
     this.setPreviousStatement(true, "configure_dataseries");
     this.setNextStatement(true, "configure_dataseries");
+    this.setTooltip('');
+    this.setColour(120);
+    this.setHelpUrl('http://matplotlib.org/1.5.3/api/markers_api.html#module-matplotlib.markers');
+  }
+};
+
+Blockly.Blocks['set_axis_ticks'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("set")
+        .appendField(new Blockly.FieldDropdown([["x", "x"], ["y", "y"]]), "AXIS")
+        .appendField("axis tick frequency to")
+        .appendField(new Blockly.FieldNumber(1), "FREQUENCY");
+    this.setPreviousStatement(true, "configure_plot");
+    this.setNextStatement(true, "configure_plot");
+    this.setTooltip('');
+    this.setHelpUrl('https://stackoverflow.com/questions/12608788/changing-the-tick-frequency-on-x-or-y-axis-in-matplotlib');
+  }
+};
+
+Blockly.Blocks['create_scale'] = {
+  init: function() {
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("create scale")
+        .appendField(new Blockly.FieldTextInput("scale one"), "SCALE_NAME")
+        .appendField("with y limits")
+        .appendField(new Blockly.FieldNumber(0), "Y_MIN")
+        .appendField(",")
+        .appendField(new Blockly.FieldNumber(10), "Y_MAX");
+    this.setPreviousStatement(true, "configure_plot");
+    this.setNextStatement(true, "configure_plot");
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['pick_scale'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("use scale")
+        .appendField(new Blockly.FieldTextInput("scale one"), "SCALE_NAME");
+    this.setPreviousStatement(true, "configure_dataseries");
+    this.setNextStatement(true, "configure_dataseries");
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['set_y_axis_limits'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Set ")
+        .appendField(new Blockly.FieldDropdown([["primary", "primary_scale"], ["secondary", "secondary_scale"]]), "SCALE")
+        .appendField("y axis limits to")
+        .appendField(new Blockly.FieldNumber(0), "LIMIT_1")
+        .appendField(",")
+        .appendField(new Blockly.FieldNumber(0), "LIMIT_2");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['configure_axis'] = {
+  init: function() {
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Configure")
+        .appendField(new Blockly.FieldDropdown([["x", "X_AXIS"], ["primary y", "PRIMARY_Y_AXIS"], ["secondary y", "SECONDARY_Y_AXIS"]]), "AXIS")
+        .appendField("axis:");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("limits:")
+        .appendField(new Blockly.FieldNumber(0), "MIN")
+        .appendField(",")
+        .appendField(new Blockly.FieldNumber(10), "MAX");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("label")
+        .appendField(new Blockly.FieldTextInput(""), "LABEL");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("tick frequency")
+        .appendField(new Blockly.FieldNumber(0), "TICK_FREQUENCY");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
--- matplotlib_generators_1.js
+++ matplotlib_generators_1.js
@@ -1,7 +1,13 @@
 Blockly.Python['create_graph'] = function(block) {
-  var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
+  var dropdown_num_y_axes = block.getFieldValue('NUM_Y_AXES');
+  var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+
   // TODO: Assemble Python into code variable.
   var code = 'figure, axes = plt.subplots()\n';
+  if (dropdown_num_y_axes == '2') {
+    code += 'secondary_scale = axes.twinx()\n';
+  }
+  code += statements_name;
   return code;
 };
 
@@ -28,35 +34,15 @@ Blockly.Python['add_data'] = function(block) {
 Blockly.Python['add_data_two'] = function(block) {
   var value_data_x = Blockly.Python.valueToCode(block, 'DATA_x', Blockly.Python.ORDER_ATOMIC);
   var value_data_y = Blockly.Python.valueToCode(block, 'DATA_Y', Blockly.Python.ORDER_ATOMIC);
+  var statements_name = Blockly.Python.statementToCode(block, 'NAME');
 
-  var statement_block = block.getInput('NAME').connection.targetBlock();
-
-  var statement_blocks_map = {};
-  while (statement_block) {
-    var type = statement_block.type;
-    if (!statement_blocks_map[type]) {
-      statement_blocks_map[type] = [];
-    }
-    statement_blocks_map[type].push(statement_block);
-    statement_block = statement_block.getNextBlock();
-  }
-
-  var modifier_string = '';
-  for (modifier in statement_blocks_map) {
-    var value = statement_blocks_map[modifier];
-    var most_recent = value[value.length - 1];
-    modifier_string += ', ' + Blockly.Python.blockToCode(most_recent);
-  }
-
-  var code = 'axes.plot(' + value_data_x + ', ' + value_data_y + modifier_string + ')\n';
-  // if (statements_name) {
-  //   if (statements_name.endsWith(' ')) {
-  //     statements_name = statements_name.slice(0, -2);
-  //   }
-  //   code = code + ', ' + statements_name;
-  // }
-
-  //code += ')\n';
+  var code = '\ndef configure_this_line():\n' + 
+      Blockly.Python.INDENT + '# Default scale, if not specified\n' +
+      Blockly.Python.INDENT + 'config_object = {}\n' +
+      Blockly.Python.INDENT + 'scale = axes\n' + 
+      statements_name +
+      Blockly.Python.INDENT + 'scale.plot(' + value_data_x + ', ' + value_data_y + ', **config_object)\n' +
+      'configure_this_line()\n\n';
   return code;
 };
 
@@ -86,24 +72,97 @@ Blockly.Python['set_axis_label'] = function(block) {
 
 Blockly.Python['dataseries_set_color'] = function(block) {
   var colour_name = block.getFieldValue('NAME');
-  var code = 'color=\'' + colour_name + '\'';
+  var code = 'config_object[\'color\'] = \'' + colour_name + '\'\n';
   return code;
 };
 
 Blockly.Python['dataseries_set_linewidth'] = function(block) {
   var number_width = block.getFieldValue('WIDTH');
-  var code = 'linewidth=' + number_width;
+  var code = 'config_object[\'linewidth\'] = ' + number_width + '\n';
   return code;
 };
 
 Blockly.Python['dataseries_set_linestyle'] = function(block) {
   var dropdown_style = block.getFieldValue('STYLE');
-  var code = 'linestyle=\'' + dropdown_style + '\'';
+  var code = 'config_object[\'linestyle\'] = \'' + dropdown_style + '\'\n';
   return code;
 };
 
 Blockly.Python['dataseries_set_marker'] = function(block) {
   var dropdown_marker = block.getFieldValue('MARKER');
-  var code = 'marker=\'' + dropdown_marker + '\'';
+  var number_marker_size = block.getFieldValue('MARKER_SIZE');
+
+  if (dropdown_marker) {
+    var marker_code = 'config_object[\'marker\'] = \'' + dropdown_marker + '\'\n';
+  }
+  if (number_marker_size) {
+    var size_code = 'config_object[\'markersize\'] = ' + number_marker_size + '\n';
+  }
+  var code;
+  if (dropdown_marker && number_marker_size) {
+    code = marker_code + size_code;
+  } else if (dropdown_marker) {
+    code = marker_code;
+  } else if (number_marker_size) {
+    code = size_code;
+  }
+  
+  return code;
+};
+
+Blockly.Python['set_axis_ticks'] = function(block) {
+  var dropdown_axis = block.getFieldValue('AXIS');
+  var number_frequency = block.getFieldValue('FREQUENCY');
+  // TODO: Import ticker
+  var code = 'axes.' + dropdown_axis +
+      'axis.set_major_locator(ticker.MultipleLocator(' + number_frequency + '))\n';
+  return code;
+};
+
+Blockly.Python['create_scale'] = function(block) {
+  var text_scale_name = block.getFieldValue('SCALE_NAME');
+  var number_y_min = block.getFieldValue('Y_MIN');
+  var number_y_max = block.getFieldValue('Y_MAX');
+
+  text_scale_name = text_scale_name.replace(' ', '_');
+
+  var code = text_scale_name + ' = axes.twinx()\n' +
+      text_scale_name + '.set_ylim(' + number_y_min + ', ' + number_y_max + ')\n';
+  return code;
+};
+
+Blockly.Python['pick_scale'] = function(block) {
+  var text_scale_name = block.getFieldValue('SCALE_NAME');
+  text_scale_name = text_scale_name.replace(' ', '_');
+  // TODO: Assemble Python into code variable.
+  var code = '# Use a non-default scale\n' +
+      'scale = ' + text_scale_name + '\n';
+  return code;
+};
+
+Blockly.Python['set_y_axis_limits'] = function(block) {
+  var dropdown_scale = block.getFieldValue('SCALE');
+  var number_limit_1 = block.getFieldValue('LIMIT_1');
+  var number_limit_2 = block.getFieldValue('LIMIT_2');
+  // TODO: Assemble Python into code variable.
+  if (dropdown_scale == 'primary_scale') {
+    var code = 'axes.set_ylim(' + number_limit_1 + ', ' + number_limit_2 + ')\n';
+  } else {
+    var code = 'if (secondary_scale == None):\n' + 
+        Blockly.Python.INDENT + 'secondary_scale = axes.twinx()\n' +
+        'secondary_scale.set_ylim(' + number_limit_1 + ', ' + number_limit_2 + ')\n';
+  }
+  return code;
+};
+
+Blockly.Python['configure_axis'] = function(block) {
+  var dropdown_axis = block.getFieldValue('AXIS');
+  var number_min = block.getFieldValue('MIN');
+  var number_max = block.getFieldValue('MAX');
+  var text_label = block.getFieldValue('LABEL');
+  var number_tick_frequency = block.getFieldValue('TICK_FREQUENCY');
+
+
+  var code = '...\n';
   return code;
 };
